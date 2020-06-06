00dfe3657c4bc12edba967df056c798128f1dc12
Vladislav <nepogodin.vlad@gmail.com>
2020-06-07 01:50:49 +0400

    CI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 wget https://github.com/json-c/json-c/archive/json-c-0.14-20200419.tar.gz
-pwd
 tar -tzvf json-c-0.14-20200419.tar.gz
-pwd
-#mkdir json-c-build
-#cd json-c-build
-#cmake ../json-c-json-c-0.14-20200419
-#make
-#make install
+mkdir json-c-build && cd json-c-build
+cmake ../~/build/ctrlcvnigerguard/Neural-Network-C/json-c-json-c-0.14-20200419
+make
+make install
