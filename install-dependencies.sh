b6cfb215aeb2059b8eaf92a26215c2b40be27d47
Vladislav <nepogodin.vlad@gmail.com>
2020-06-07 01:24:37 +0400

    CI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,9 +2,6 @@
 
 sudo apt install cmake
 wget https://github.com/json-c/json-c/archive/json-c-0.14-20200419.tar.gz
-tar -tzvf json-c-0.14-20200419.tar.gz
-mkdir json-c-build
-cd json-c-build
-cmake ../json-c-json-c-0.14-20200419
+tar -tzvf json-c-0.14-20200419.tar.gz && mkdir json-c-build && cd json-c-build && cmake ../json-c-json-c-0.14-20200419
 make
 make install
