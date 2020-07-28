e0c3acfee78a3eafda1bb35f5358b54bba314685
Vladislav <nepogodin.vlad@gmail.com>
2020-07-29 00:09:45 +0400

    CI

--- install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/json-c/json-c.git -b json-c-0.14-20200419 ../../json-c
-mkdir json-c-build && cd json-c-build
-cmake ../../../json-c -DCMAKE_BUILD_TYPE="release" -DBUILD_STATIC_LIBS=OFF
-make
-sudo make install
