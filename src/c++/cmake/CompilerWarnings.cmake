fc8945ad5c1fd8d82e5bdcb0a04c7f9bd1a516a4
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2021-09-09 02:07:57 +0400

    cleanup cmake

--- src/c++/cmake/CompilerWarnings.cmake
+++ src/c++/cmake/CompilerWarnings.cmake
@@ -1,5 +1,5 @@
 function(set_project_warnings project_name)
-    option(WARNINGS_AS_ERROR "Treat compiler warnings as error" FALSE)
+    option(WARNINGS_AS_ERROR "Treat compiler warnings as error" TRUE)
 
     set(MSVC_WARNINGS
         /W4 # Base
